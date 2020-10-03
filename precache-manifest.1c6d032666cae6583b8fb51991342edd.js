a318a36b2efdc2b9f471052d563de7c9027f5b6c
jmekstrom <jmekstrom@gmail.com>
2020-10-03 10:40:39 -0700

    Updates

---
+++ precache-manifest.1c6d032666cae6583b8fb51991342edd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c5ce33ae549ec72216a26ca8889e14c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5148a4c41234b586b6b5",
+    "url": "/monsters-rolodex/static/css/main.6e7bc387.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5148a4c41234b586b6b5",
+    "url": "/monsters-rolodex/static/js/main.036a618d.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
